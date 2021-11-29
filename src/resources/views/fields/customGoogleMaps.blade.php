e9f949724f622ac79f76416ba7364af49c27a9a4
Ahmed Safaa <ahmed@sz4h.com>
2021-11-29 11:24:23 +0300

    Fix

--- src/resources/views/fields/customGoogleMaps.blade.php
+++ src/resources/views/fields/customGoogleMaps.blade.php
@@ -3,117 +3,119 @@
 <div>
     <label>{!! $field['label'] !!}</label>
     <input type="text" id="{{ $field['name'] }}" name="{{ $field['name'] }}"
-        value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
+           value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
             @include('crud::fields.inc.attributes')>
-    <input type="hidden" name="address_latitude" id="address-latitude" value="0" />
-    <input type="hidden" name="address_longitude" id="address-longitude" value="0" />
+    <input type="hidden" name="address_latitude" id="address-latitude" value="0"/>
+    <input type="hidden" name="address_longitude" id="address-longitude" value="0"/>
     <div id="address-map-container" style="width:100%;height:400px; ">
         <div style="width: 100%; height: 100%" id="address-map"></div>
     </div>
 
     {{-- HINT --}}
     @if (isset($field['hint']))
-    <p class="help-block">{!! $field['hint'] !!}</p>
+        <p class="help-block">{!! $field['hint'] !!}</p>
     @endif
 </div>
 @include('crud::fields.inc.wrapper_end')
 @if ($crud->checkIfFieldIsFirstOfItsType($field, $fields))
-{{-- FIELD EXTRA CSS  --}}
-{{-- push things in the after_styles section --}}
+    {{-- FIELD EXTRA CSS  --}}
+    {{-- push things in the after_styles section --}}
 
-@push('crud_fields_styles')
-<!-- no styles -->
-@endpush
+    @push('crud_fields_styles')
+        <!-- no styles -->
+    @endpush
 
-{{-- FIELD EXTRA JS --}}
-{{-- push things in the after_scripts section --}}
+    {{-- FIELD EXTRA JS --}}
+    {{-- push things in the after_scripts section --}}
 
-@push('crud_fields_scripts')
-<!-- no scripts -->
-<script
-    src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&libraries=places&callback=initialize" async defer></script>
-    <script>
-        function initialize() {
+    @push('crud_fields_scripts')
+        <!-- no scripts -->
+        <script
+                src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&libraries=places&callback=initialize"
+                async defer></script>
+        <script>
+            function initialize() {
 
-            $('form').on('keyup keypress', function(e) {
-                var keyCode = e.keyCode || e.which;
-                if (keyCode === 13) {
-                    e.preventDefault();
-                    return false;
-                }
-            });
-            const locationInputs = document.getElementsByClassName("map-input");
-
-            const autocompletes = [];
-            const geocoder = new google.maps.Geocoder;
-            for (let i = 0; i < locationInputs.length; i++) {
-
-                const input = locationInputs[i];
-                const fieldKey = input.id.replace("-input", "");
-                const isEdit = document.getElementById(fieldKey + "-latitude").value != '' && document.getElementById(fieldKey + "-longitude").value != '';
-
-                const latitude = parseFloat(document.getElementById(fieldKey + "-latitude").value) || -33.8688;
-                const longitude = parseFloat(document.getElementById(fieldKey + "-longitude").value) || 151.2195;
-
-                const map = new google.maps.Map(document.getElementById(fieldKey + '-map'), {
-                    center: {lat: latitude, lng: longitude},
-                    zoom: 13
+                $('form').on('keyup keypress', function (e) {
+                    var keyCode = e.keyCode || e.which;
+                    if (keyCode === 13) {
+                        e.preventDefault();
+                        return false;
+                    }
                 });
-                const marker = new google.maps.Marker({
-                    map: map,
-                    position: {lat: latitude, lng: longitude},
-                });
+                const locationInputs = document.getElementsByClassName("map-input");
 
-                marker.setVisible(isEdit);
+                const autocompletes = [];
+                const geocoder = new google.maps.Geocoder;
+                for (let i = 0; i < locationInputs.length; i++) {
 
-                const autocomplete = new google.maps.places.Autocomplete(input);
-                autocomplete.key = fieldKey;
-                autocompletes.push({input: input, map: map, marker: marker, autocomplete: autocomplete});
-            }
+                    const input = locationInputs[i];
+                    const fieldKey = input.id.replace("-input", "");
+                    console.log(document.getElementById(fieldKey + "-latitude-hidden").value);
+                    const isEdit = document.getElementById(fieldKey + "-latitude-hidden").value != '' && document.getElementById(fieldKey + "-longitude-hidden").value != '';
 
-            for (let i = 0; i < autocompletes.length; i++) {
-                const input = autocompletes[i].input;
-                const autocomplete = autocompletes[i].autocomplete;
-                const map = autocompletes[i].map;
-                const marker = autocompletes[i].marker;
+                    const latitude = parseFloat(document.getElementById(fieldKey + "-latitude-hidden").value) || -33.8688;
+                    const longitude = parseFloat(document.getElementById(fieldKey + "-longitude-hidden").value) || 151.2195;
 
-                google.maps.event.addListener(autocomplete, 'place_changed', function () {
-                    marker.setVisible(false);
-                    const place = autocomplete.getPlace();
-
-                    geocoder.geocode({'placeId': place.place_id}, function (results, status) {
-                        if (status === google.maps.GeocoderStatus.OK) {
-                            const lat = results[0].geometry.location.lat();
-                            const lng = results[0].geometry.location.lng();
-                            setLocationCoordinates(autocomplete.key, lat, lng);
-                        }
+                    const map = new google.maps.Map(document.getElementById(fieldKey + '-map'), {
+                        center: {lat: latitude, lng: longitude},
+                        zoom: 13
+                    });
+                    const marker = new google.maps.Marker({
+                        map: map,
+                        position: {lat: latitude, lng: longitude},
                     });
 
-                    if (!place.geometry) {
-                        window.alert("No details available for input: '" + place.name + "'");
-                        input.value = "";
-                        return;
-                    }
+                    marker.setVisible(isEdit);
 
-                    if (place.geometry.viewport) {
-                        map.fitBounds(place.geometry.viewport);
-                    } else {
-                        map.setCenter(place.geometry.location);
-                        map.setZoom(17);
-                    }
-                    marker.setPosition(place.geometry.location);
-                    marker.setVisible(true);
+                    const autocomplete = new google.maps.places.Autocomplete(input);
+                    autocomplete.key = fieldKey;
+                    autocompletes.push({input: input, map: map, marker: marker, autocomplete: autocomplete});
+                }
 
-                });
+                for (let i = 0; i < autocompletes.length; i++) {
+                    const input = autocompletes[i].input;
+                    const autocomplete = autocompletes[i].autocomplete;
+                    const map = autocompletes[i].map;
+                    const marker = autocompletes[i].marker;
+
+                    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                        marker.setVisible(false);
+                        const place = autocomplete.getPlace();
+
+                        geocoder.geocode({'placeId': place.place_id}, function (results, status) {
+                            if (status === google.maps.GeocoderStatus.OK) {
+                                const lat = results[0].geometry.location.lat();
+                                const lng = results[0].geometry.location.lng();
+                                setLocationCoordinates(autocomplete.key, lat, lng);
+                            }
+                        });
+
+                        if (!place.geometry) {
+                            window.alert("No details available for input: '" + place.name + "'");
+                            input.value = "";
+                            return;
+                        }
+
+                        if (place.geometry.viewport) {
+                            map.fitBounds(place.geometry.viewport);
+                        } else {
+                            map.setCenter(place.geometry.location);
+                            map.setZoom(17);
+                        }
+                        marker.setPosition(place.geometry.location);
+                        marker.setVisible(true);
+
+                    });
+                }
             }
-        }
 
-        function setLocationCoordinates(key, lat, lng) {
-            const latitudeField = document.getElementById(key + "-" + "latitude");
-            const longitudeField = document.getElementById(key + "-" + "longitude");
-            latitudeField.value = lat;
-            longitudeField.value = lng;
-        }
-    </script>
-@endpush
+            function setLocationCoordinates(key, lat, lng) {
+                const latitudeField = document.getElementById(key + "-" + "latitude");
+                const longitudeField = document.getElementById(key + "-" + "longitude");
+                latitudeField.value = lat;
+                longitudeField.value = lng;
+            }
+        </script>
+    @endpush
 @endif
